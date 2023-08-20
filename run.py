abdb85a2f0979dee96b11c4931080067db04c97d
mukuyo <adgjmptw47777@gmail.com>
2023-08-20 16:00:53 +0900

    fix detect

--- run.py
+++ run.py
@@ -1,20 +1,8 @@
+from object_detection.detect import run_detect
 from pose_estimate.src.predict import run_predict
-from ultralytics import YOLO
 
-model = YOLO(model='./runs/detect/train/weights/best.pt', task='detect')
-results = model.predict(
-   source='./data/pipe/test/images/1000.png',
-   save=True,
-   conf=0.25,
-)
-for r in results:
-    
-    # annotator = Annotator(frame)
-    
-    boxes = r.boxes
-    for box in boxes:
-        
-        b = box.xyxy[0]  # get box coordinates in (top, left, bottom, right) format
-        c = box.cls
-        print(b, c)
-run_predict()
+if __name__ == "__main__":
+    run_detect(model_path='./runs/detect/train/weights/best.pt', 
+            img_path='./data/pipe/test/images/1000.png')
+
+    run_predict()
